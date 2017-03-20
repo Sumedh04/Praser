7c6b1ace861d36aafa9e38c9d778f8d116a3b071
Sumedh04 <sumedhm14@gmail.com>
2017-03-21 00:08:43 +0530

    Update Easy 2.R

--- Easy 2.R
+++ Easy 2.R
@@ -1,24 +1,22 @@
-genusname <- function(input_url,...)
-{
-  library(RCurl)
-  library(XML)
-  library(rvest)
-  library(httr)
-  
-  set_config(use_proxy(url= "10.3.100.207",port = 8080))
-  
-  #Easy: Part 1
-  #Read html webpage
-  
-  url <- input_url #we get the url from the user into the function
-  webpage <- readLines(url)
-  
-  # Getting the genus name
-  genus_name <- grep('genus=',webpage)
-  raw_genus <- webpage[genus_name]
-  
-  # Cleaning data genus names for Output
-  genus_name_clean <- sub(".*?<i>(.*?)</i>.*", "\\1", raw_genus)
-  genus_name_clean
-  
-}
+#Easy: List out all the species from the list [https://www.abdb-africa.org/genus/Papilio]
+
+library(RCurl)
+library(XML)
+library(rvest)
+library(httr)
+
+set_config(use_proxy(url= "10.3.100.207",port = 8080))
+
+#Easy: Part 1
+#Read html webpage
+
+url <- "https://www.abdb-africa.org/genus/Papilio"
+webpage <- readLines(url)
+
+# Getting the genus name
+species_name <- grep('species=',webpage)
+raw_species <- webpage[species_name]
+
+# Cleaning data genus names for Output
+species_name_clean <- sub(".*?<i>(.*?)</i>.*", "\\1", raw_species)
+species_name_clean
